6d00ec058a3c2c6515f987889d58dbd632981748
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-02-08 13:10:47 -0500

    Finished Hello World International

--- HelloWorldIntConnorQ/HelloWorldIntConnorQ/HelloWorldIntForm.Designer.cs
+++ HelloWorldIntConnorQ/HelloWorldIntConnorQ/HelloWorldIntForm.Designer.cs
@@ -28,20 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblGreetings = new System.Windows.Forms.Label();
+            this.grbLanguages = new System.Windows.Forms.GroupBox();
+            this.radEnglish = new System.Windows.Forms.RadioButton();
+            this.radFrench = new System.Windows.Forms.RadioButton();
+            this.radGerman = new System.Windows.Forms.RadioButton();
+            this.radRussian = new System.Windows.Forms.RadioButton();
+            this.mnuExit = new System.Windows.Forms.MenuStrip();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.grbLanguages.SuspendLayout();
+            this.mnuExit.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblGreetings
+            // 
+            this.lblGreetings.AutoSize = true;
+            this.lblGreetings.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblGreetings.Location = new System.Drawing.Point(40, 60);
+            this.lblGreetings.Name = "lblGreetings";
+            this.lblGreetings.Size = new System.Drawing.Size(70, 26);
+            this.lblGreetings.TabIndex = 0;
+            this.lblGreetings.Text = "label1";
+            this.lblGreetings.Click += new System.EventHandler(this.lblGreetings_Click);
+            // 
+            // grbLanguages
+            // 
+            this.grbLanguages.Controls.Add(this.radRussian);
+            this.grbLanguages.Controls.Add(this.radGerman);
+            this.grbLanguages.Controls.Add(this.radFrench);
+            this.grbLanguages.Controls.Add(this.radEnglish);
+            this.grbLanguages.Location = new System.Drawing.Point(39, 127);
+            this.grbLanguages.Name = "grbLanguages";
+            this.grbLanguages.Size = new System.Drawing.Size(198, 141);
+            this.grbLanguages.TabIndex = 1;
+            this.grbLanguages.TabStop = false;
+            this.grbLanguages.Text = "Languages";
+            this.grbLanguages.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radEnglish
+            // 
+            this.radEnglish.AutoSize = true;
+            this.radEnglish.Location = new System.Drawing.Point(6, 19);
+            this.radEnglish.Name = "radEnglish";
+            this.radEnglish.Size = new System.Drawing.Size(59, 17);
+            this.radEnglish.TabIndex = 0;
+            this.radEnglish.TabStop = true;
+            this.radEnglish.Text = "English";
+            this.radEnglish.UseVisualStyleBackColor = true;
+            this.radEnglish.CheckedChanged += new System.EventHandler(this.radEnglish_CheckedChanged);
+            // 
+            // radFrench
+            // 
+            this.radFrench.AutoSize = true;
+            this.radFrench.Location = new System.Drawing.Point(6, 63);
+            this.radFrench.Name = "radFrench";
+            this.radFrench.Size = new System.Drawing.Size(58, 17);
+            this.radFrench.TabIndex = 1;
+            this.radFrench.TabStop = true;
+            this.radFrench.Text = "French";
+            this.radFrench.UseVisualStyleBackColor = true;
+            this.radFrench.CheckedChanged += new System.EventHandler(this.radFrench_CheckedChanged);
+            // 
+            // radGerman
+            // 
+            this.radGerman.AutoSize = true;
+            this.radGerman.Location = new System.Drawing.Point(101, 19);
+            this.radGerman.Name = "radGerman";
+            this.radGerman.Size = new System.Drawing.Size(62, 17);
+            this.radGerman.TabIndex = 2;
+            this.radGerman.TabStop = true;
+            this.radGerman.Text = "German";
+            this.radGerman.UseVisualStyleBackColor = true;
+            this.radGerman.CheckedChanged += new System.EventHandler(this.radGerman_CheckedChanged);
+            // 
+            // radRussian
+            // 
+            this.radRussian.AutoSize = true;
+            this.radRussian.Location = new System.Drawing.Point(100, 63);
+            this.radRussian.Name = "radRussian";
+            this.radRussian.Size = new System.Drawing.Size(63, 17);
+            this.radRussian.TabIndex = 3;
+            this.radRussian.TabStop = true;
+            this.radRussian.Text = "Russian";
+            this.radRussian.UseVisualStyleBackColor = true;
+            this.radRussian.CheckedChanged += new System.EventHandler(this.radRussian_CheckedChanged);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem});
+            this.mnuExit.Location = new System.Drawing.Point(0, 0);
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(289, 24);
+            this.mnuExit.TabIndex = 2;
+            this.mnuExit.Text = "Program";
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
             // 
             // frmHelloWorldInt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(289, 341);
+            this.Controls.Add(this.grbLanguages);
+            this.Controls.Add(this.lblGreetings);
+            this.Controls.Add(this.mnuExit);
+            this.MainMenuStrip = this.mnuExit;
             this.Name = "frmHelloWorldInt";
             this.Text = "Hello World International By Connor Quinlan";
+            this.grbLanguages.ResumeLayout(false);
+            this.grbLanguages.PerformLayout();
+            this.mnuExit.ResumeLayout(false);
+            this.mnuExit.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblGreetings;
+        private System.Windows.Forms.GroupBox grbLanguages;
+        private System.Windows.Forms.RadioButton radRussian;
+        private System.Windows.Forms.RadioButton radGerman;
+        private System.Windows.Forms.RadioButton radFrench;
+        private System.Windows.Forms.RadioButton radEnglish;
+        private System.Windows.Forms.MenuStrip mnuExit;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
     }
 }
 
--- HelloWorldIntConnorQ/HelloWorldIntConnorQ/HelloWorldIntForm.cs
+++ HelloWorldIntConnorQ/HelloWorldIntConnorQ/HelloWorldIntForm.cs
@@ -16,5 +16,40 @@ namespace HelloWorldIntConnorQ
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreetings.Text = "Hello, World!";
+        }
+
+        private void radFrench_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreetings.Text = "Bonjour, Le Monde!";
+        }
+
+        private void radGerman_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreetings.Text = "Hallo, Welt!";
+        }
+
+        private void radRussian_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreetings.Text = "Привет, мир!";
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblGreetings_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
