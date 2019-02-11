64373d63ed6e2b3b48dd348df13b2933def3b4ff
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-02-11 12:23:45 -0500

    Created PerAreaVol

---
+++ PerAreaVolConnorQ/PerAreaVolConnorQ/PerAreaVolForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PerAreaVolConnorQ
+{
+    partial class frmPerAreaVol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmPerAreaVol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 592);
+            this.Name = "frmPerAreaVol";
+            this.Text = "Perimeter, Area, Volume by Connor Quinlan";
+            this.Load += new System.EventHandler(this.PerAreaVolForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
