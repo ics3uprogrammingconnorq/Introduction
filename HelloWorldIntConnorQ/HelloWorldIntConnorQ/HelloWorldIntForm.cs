f23bb34d56102fd54bb1c18e36b7a9f19c07ebf7
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-02-18 21:26:14 -0500

    completed hello world international

--- HelloWorldIntConnorQ/HelloWorldIntConnorQ/HelloWorldIntForm.cs
+++ HelloWorldIntConnorQ/HelloWorldIntConnorQ/HelloWorldIntForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 18-02-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #6 Hello World International
+ * This program allows the user to pick a language in which the program will display hello world in that language.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,26 +30,31 @@ namespace HelloWorldIntConnorQ
 
         }
 
+        // When English is pressed change language to English
         private void radEnglish_CheckedChanged(object sender, EventArgs e)
         {
             lblGreetings.Text = "Hello, World!";
         }
 
+        // When French is pressed change language to French
         private void radFrench_CheckedChanged(object sender, EventArgs e)
         {
             lblGreetings.Text = "Bonjour, Le Monde!";
         }
 
+        // When German is pressed change language to German
         private void radGerman_CheckedChanged(object sender, EventArgs e)
         {
             lblGreetings.Text = "Hallo, Welt!";
         }
 
+        // When Russian is pressed change language to Russian
         private void radRussian_CheckedChanged(object sender, EventArgs e)
         {
             lblGreetings.Text = "Привет, мир!";
         }
 
+        // When Exit is pressed close the program
         private void mniExit_Click(object sender, EventArgs e)
         {
             this.Close();
